604418ef33ca1d5600cb668248a4d80f04ccdf6c
02sandro07 <sa5679@mci4me.at>
2024-01-08 13:38:24 +0100

    changed back and added selectbox

--- main1.py
+++ main1.py
@@ -6,13 +6,13 @@ st.sidebar.image('MCI.png', width=300)
 st.sidebar.title("Hochschulgeräteverwaltung")
 
 # Erstelle die Tabs in der Sidebar
-selected_tab = st.sidebar.radio("Navigation", ["Geräte", "Nutzer", "Reservierungen", "Wartung"])
+#selected_tab = st.sidebar.radio("Navigation", ["Geräte", "Nutzer", "Reservierungen", "Wartung"]) #statt radio was anderes nehmen
+selected_tab = st.sidebar.selectbox("Navigation", ["Geräte", "Nutzer", "Reservierungen", "Wartung"]) #statt radio was anderes nehmen
 
-# Größere Überschrift in der Sidebar
-st.sidebar.markdown("### Ansichtsoptionen")
 
 # Geräte-Tab
 if selected_tab == "Geräte":
+    #st.image('MCI.png', width=400, caption='MCI Logo: https://www.mci.edu/de/medien/logos-bilder')
     st.header("Neues Gerät anlegen oder bestehendes Gerät ändern")
     device_name = st.text_input("Name des Geräts:")
     device_responsible = st.text_input("Verantwortliche Person:")
@@ -28,6 +28,8 @@ if selected_tab == "Geräte":
 
 # Nutzer-Tab
 elif selected_tab == "Nutzer":
+    #st.image('MCI.png', width=400, caption='MCI Logo: https://www.mci.edu/de/medien/logos-bilder')
+
     st.header("Neuen Nutzer anlegen")
     user_email = st.text_input("E-Mail-Adresse des Nutzers:")
     user_name = st.text_input("Name des Nutzers:")
@@ -37,6 +39,8 @@ elif selected_tab == "Nutzer":
 
 # Reservierungen-Tab
 elif selected_tab == "Reservierungen":
+    #st.image('MCI.png', width=400, caption='MCI Logo: https://www.mci.edu/de/medien/logos-bilder')
+
     st.header("Reservierung anlegen oder entfernen")
     # Erstelle ein Dropdown-Menü mit allen Geräten
     device_name = st.selectbox("Gerät:", ["Gerät 1", "Gerät 2", "Gerät 3"])
@@ -52,11 +56,16 @@ elif selected_tab == "Reservierungen":
 
 # Wartung-Tab
 elif selected_tab == "Wartung":
+    # Anzeige des MCI-Logos in der Hauptseite
+    #st.image('MCI.png', width=400, caption='MCI Logo: https://www.mci.edu/de/medien/logos-bilder')
+
     st.header("Wartungsinformationen")
     if st.button("Wartungsinformationen anzeigen"):
         # dosomething
         st.success("Wartungsinformationen wurden angezeigt!")
 
-# Größere Überschrift in der Sidebar
-st.sidebar.markdown("### Quelle")
-st.sidebar.markdown("Quelle MCI-Logo: [MCI Logo](https://www.mci.edu/de/medien/logos-bilder)")
+
+#st.sidebar.text("Quelle MCI-Logo: https://www.mci.edu/de/medien/logos-bilder") 
+#st.sidebar._text_input("Quelle MCI-Logo: https://www.mci.edu/de/medien/logos-bilder")
+st.sidebar.markdown("Quelle MCI-Logo: https://www.mci.edu/de/medien/logos-bilder")
+
