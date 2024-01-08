9752af39fc034b8ecfc2487994b632b21da9b7de
02sandro07 <sa5679@mci4me.at>
2024-01-08 13:49:33 +0100

    added Artikelnummer, Anschaffungsdatum, optinale Beschreibung

--- main1.py
+++ main1.py
@@ -2,82 +2,81 @@ import streamlit as st
 from datetime import datetime
 import time
 
-# Diese bild soll rechts angezeigt werden
+# Diese Bild soll rechts angezeigt werden
 st.sidebar.image('MCI.png', width=300)
 st.sidebar.title("Hochschulgeräteverwaltung")
 
-# Erstelle die Tabs in der Sidebar
-#selected_tab = st.sidebar.radio("Navigation", ["Geräte", "Nutzer", "Reservierungen", "Wartung"]) 
-selected_tab = st.sidebar.selectbox("Navigation", ["Geräte", "Nutzer", "Reservierungen", "Wartung"]) #verschiedene varianten statt selectbox
+# Verschiedene Tab Typen
 
-
-
+selected_tab = st.sidebar.selectbox("Navigation", ["Geräte", "Nutzer", "Reservierungen", "Wartung"])
 
 # Geräte-Tab
 if selected_tab == "Geräte":
     
-    #st.image('MCI.png', width=400, caption='MCI Logo: https://www.mci.edu/de/medien/logos-bilder')
     st.header("Neues Gerät anlegen oder bestehendes Gerät ändern")
+    
+    # Eingabefelder für Geräteinformationen
     device_name = st.text_input("Name des Geräts:")
-    device_responsible = st.text_input("Verantwortliche Person:")
+    article_number = st.text_input("Artikelnummer:")
+    acquisition_date = st.date_input("Anschaffungsdatum:")
+    device_description = st.text_area("Optionale Beschreibung:")
+    
     if st.button("Gerät anlegen/ändern"):
         device_data = {
             "name": device_name,
-            "responsible_person": device_responsible,
+            "article_number": article_number,
+            "acquisition_date": acquisition_date,
+            "description": device_description,
             "__creation_date": datetime.now(),
             # Weitere Attribute hier hinzufügen
         }
-        # dosomething
+        # Dosomething
         with st.spinner("Loading..."):
             time.sleep(1)
-        st.success(f"Gerät {device_name} mit dem Verantwortlichen {device_responsible} wurde angelegt/aktualisiert!")
+        st.success(f"Gerät {device_name} mit Artikelnummer {article_number} wurde angelegt/aktualisiert!")
+
 
 # Nutzer-Tab
 elif selected_tab == "Nutzer":
-    #st.image('MCI.png', width=400, caption='MCI Logo: https://www.mci.edu/de/medien/logos-bilder')
-
     st.header("Neuen Nutzer anlegen")
     user_email = st.text_input("E-Mail-Adresse des Nutzers:")
     user_name = st.text_input("Name des Nutzers:")
+    
     if st.button("Nutzer anlegen"):
-        # dosomething
+        # Dosomething
         with st.spinner("Loading..."):
             time.sleep(1)
         st.success(f"Nutzer {user_name} ({user_email}) wurde angelegt!")
 
+
 # Reservierungen-Tab
 elif selected_tab == "Reservierungen":
-    #st.image('MCI.png', width=400, caption='MCI Logo: https://www.mci.edu/de/medien/logos-bilder')
-
     st.header("Reservierung anlegen oder entfernen")
     # Erstelle ein Dropdown-Menü mit allen Geräten
     device_name = st.selectbox("Gerät:", ["Gerät 1", "Gerät 2", "Gerät 3"])
     reservation_date = st.date_input("Reservierungsdatum:")
+    
     if st.button("Reservierung anlegen/entfernen"):
         reservation_data = {
             "device_id": device_name,
             "reservation_date": reservation_date,
             # Weitere Reservierungsattribute hier hinzufügen
         }
-        # dosomething
+        # Dosomething
         with st.spinner("Loading..."):
             time.sleep(1)
         st.success(f"Reservierung für {device_name} wurde für den {reservation_date} angelegt/entfernt!")
 
+
 # Wartung-Tab
 elif selected_tab == "Wartung":
-    # Anzeige des MCI-Logos in der Hauptseite
-    #st.image('MCI.png', width=400, caption='MCI Logo: https://www.mci.edu/de/medien/logos-bilder')
-
     st.header("Wartungsinformationen")
+    
     if st.button("Wartungsinformationen anzeigen"):
-        # dosomething
+        # Dosomething
         with st.spinner("Loading..."):
             time.sleep(1)
         st.success("Wartungsinformationen wurden angezeigt!")
 
-
-#st.sidebar.text("Quelle MCI-Logo: https://www.mci.edu/de/medien/logos-bilder") 
-#st.sidebar._text_input("Quelle MCI-Logo: https://www.mci.edu/de/medien/logos-bilder")
+# Anzeige des MCI-Logos in der Sidebar
 st.sidebar.markdown("Quelle MCI-Logo: https://www.mci.edu/de/medien/logos-bilder")
-
